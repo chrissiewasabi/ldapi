f269fae5154792c53213804fddfa5271a19f1dac
Toby Batch <tobias@neontribe.co.uk>
2013-06-11 10:45:46 +0100

    Corrected paths in depoly.sh

--- app/deploy.sh
+++ app/deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 CWD=`dirname $0`/..
-PWD=`pwd`
 
 mkdir -p $HOME/bin
 
@@ -9,6 +8,4 @@ curl -sS https://getcomposer.org/installer | php -- --install-dir=$HOME/bin
 php $HOME/bin/composer.phar --working-dir=$CWD update
 php $HOME/bin/composer.phar --working-dir=$CWD update
 # sudo $HOME/bin//app/reset-permissions -y $USER $GROUP www-data www-data
-sudo $HOME/bin//app/reset-permissions
-
-cd -
+sudo $CWD/app/reset-permissions
