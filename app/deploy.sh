f861c8511a66cbfdf2792d2bf7f63177ca5c3cb3
Toby Batch <tobias@neontribe.co.uk>
2013-06-11 09:18:44 +0100

    Fixed no relative dirs

--- app/deploy.sh
+++ app/deploy.sh
@@ -3,10 +3,12 @@
 CWD=`dirname $0`/..
 PWD=`pwd`
 
-curl -sS https://getcomposer.org/installer | php
-php ./composer.phar --workinf-dir $CWD update
-php ./composer.phar --workinf-dir $CWD update
-# sudo ./app/reset-permissions -y $USER $GROUP www-data www-data
-sudo ./app/reset-permissions
+mkdir -p $HOME/bin
+
+curl -sS https://getcomposer.org/installer | php -- --install-dir=$HOME/bin
+php $HOME/bin/composer.phar --working-dir=$CWD update
+php $HOME/bin/composer.phar --working-dir=$CWD update
+# sudo $HOME/bin//app/reset-permissions -y $USER $GROUP www-data www-data
+sudo $HOME/bin//app/reset-permissions
 
 cd -
