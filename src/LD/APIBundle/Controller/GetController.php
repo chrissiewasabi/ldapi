2263a91f2244394339b2139087926d0232349b44
Tim Davies <tim@practicalparticipation.co.uk>
2013-06-15 21:37:48 +0100

    Adding search route

--- src/LD/APIBundle/Controller/GetController.php
+++ src/LD/APIBundle/Controller/GetController.php
@@ -132,4 +132,48 @@ class GetController extends APIController
 
         return $this->response($response);
     }
+    
+     /**
+     * @param string $graph  the graph to use, see service.yml
+     * @param string $object documents|assets|countries|themes|organisations|region
+     *
+     * @Route(
+     *      "/{graph}/search/{object}",
+     *      requirements={
+     *          "object" = "documents|assets|countries|themes|organisations|region",
+     *      }
+     * )
+     * @Method({"GET", "HEAD", "OPTIONS"})
+     * @return Response
+     */
+    public function searchAction($graph, $object)
+    {
+        // get and set  the query factory
+        $querybuilders = $this->container->getParameter('querybuilder');
+        if (isset($querybuilders['get_all'][$object])) {
+            $builder = $querybuilders['get_all'][$object];
+        } elseif (isset($querybuilders['default'])) {
+            $builder = $querybuilders['default'];
+        } else {
+            $builder = 'LD\APIBundle\Services\ids\DefaultQueryBuilder';
+        }
+
+        // get the sparql
+        $spqls = $this->container->getParameter('sparqls');
+        $this->container->get('logger')->info(
+            sprintf('Fetching sparql: get->%s', $object)
+        );
+        $spql = $spqls['get_all'][$object];
+
+        // fetch factory
+        $entfactories = $this->container->getParameter('factories');
+        $this->container->get('logger')->info(
+            sprintf('Fetching factory: get->%s', $object)
+        );
+        $factoryClass = $entfactories['get'][$object];
+
+        $response = $this->chomp($graph, $spql, $factoryClass, $builder);
+
+        return $this->response($response);
+    }
 }
